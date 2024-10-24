a330fb4871c56b7873276159314c9e77d1af4866
F2005-rgb <yaurichamorrofiorela@gmail.com>
2024-10-24 12:15:08 -0500

    mi-primer commit

---
+++ ejercicio_2/ejer.js
@@ -0,0 +1,41 @@
+//variebles
+//var 
+var a =1;
+var a =2;
+console.log(a)
+//let mas recoemndado 
+let b =1
+//const
+const pi =3.1416
+const meses = 12
+
+var nombre ="Juan"
+var edad = 23
+var nombre1 ='Pedro'
+nombre = nombre1
+console.log(nombre)
+
+
+// areea de un cuadrado
+var lado =5;
+var area = (lado * lado) 
+console.log("El area del cuadrado es:" + area);
+
+//area del triangulo
+let base =5;
+let altura =10;
+let resp = (base * altura) /2;
+console.log("El area del triangulo es:" + resp);
+
+//area del rectangulo
+
+let m = 6;
+let f = 12;
+let at=(m * f);
+console.log("El area del rectangulo es:" + at)
+
+//area del rombo
+let D = 10;
+let d = 8;
+let resultado =(D*d/2);
+console.log("El area del rombo es:"+ resultado);
